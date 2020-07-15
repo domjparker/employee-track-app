e1846a1ffedab2b2210dbb13f72d49bc16cacb2d
Rees Dart <thedominicparker@gmail.com>
2020-07-14 21:11:37 -0700

    schema.sql and seeds.sql set up.

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeTrack_db;
+CREATE DATABASE employeeTrack_db;
+USE employeeTrack_db;
+
+CREATE TABLE departments(
+    id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE roles(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(5,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments (id)
+);
+
+
+CREATE TABLE employees(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NUll,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
